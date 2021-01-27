64180e01472c57cdff7d0abcd8849a9946a84b1e
Moon Kyung Song <moonsont98@postech.ac.kr>
2021-01-27 17:27:41 +0900

    Add Design of creatin account section in adminmanagement page

--- src/pages/AdminManagementPage.tsx
+++ src/pages/AdminManagementPage.tsx
@@ -1,15 +1,31 @@
 import React, { useContext, useState } from 'react';
-import { Button, TextField } from '@material-ui/core';
+import { Button, Paper, TextField } from '@material-ui/core';
+import { createStyles, fade, makeStyles, Theme } from '@material-ui/core/styles';
 import { RestaurantOwnerRegister } from '../types/AuthTypes';
 import axios from 'axios';
 import UserContext from '../contexts/UserContext';
 import { SERVER_URL } from '../config';
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		createAccount: {
+			display: 'flex',
+			flexDirection: 'column',
+			width: '40%',
+			padding: '1.5rem',
+			// justifyContent: 'space-between',
+			// marginLeft: '20%',
+			// marginRight: '20%',
+		},
+	})
+);
 
 function AdminManagementPage() {
 	const [restaurantOwnerRegisterInformation, setRestaurantOnwerRegisterInformation] = useState<RestaurantOwnerRegister>(
 		{ registerNumber: '', password: '' }
 	);
 	const [password, setPassword] = useState('');
+	const classes = useStyles();
 
 	const { userStatus } = useContext(UserContext);
 
@@ -30,20 +46,22 @@ function AdminManagementPage() {
 	return (
 		<form onSubmit={createRestaurantOwnerAccount}>
 			<div>
-				<TextField
-					label="사업자등록번호"
-					placeholder="사업자등록번호를 입력하시오"
-					value={restaurantOwnerRegisterInformation.registerNumber}
-					onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-						setRestaurantOnwerRegisterInformation({
-							...restaurantOwnerRegisterInformation,
-							registerNumber: event.target.value,
-						});
-					}}
-				/>
+				<Paper className={classes.createAccount}>
+					<TextField
+						label="사업자등록번호"
+						placeholder="사업자등록번호를 입력하시오"
+						value={restaurantOwnerRegisterInformation.registerNumber}
+						onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+							setRestaurantOnwerRegisterInformation({
+								...restaurantOwnerRegisterInformation,
+								registerNumber: event.target.value,
+							});
+						}}
+					/>
+					<TextField disabled label="비밀번호" placeholder="생성된 비밀번호" value={password} />
+					<Button type="submit">Create New Restaurant Owner's Account</Button>
+				</Paper>
 			</div>
-			{password.length > 0 && password}
-			<Button type="submit">Create New Restaurant Owner's Account</Button>
 		</form>
 	);
 }
